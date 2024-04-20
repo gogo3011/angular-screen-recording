64a1d914f08cc026a070d6636970c16752a6a567
Georgi Dimitrov <georgedimtrov92@gmail.com>
2024-04-20 16:42:07 +0300

    Build page commit

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withDebugTracing } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations' 
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()]
-};
